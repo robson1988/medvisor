d95514422fe23e691ead759bf9dca7254d3ca3db
RBcode <40271285+robson1988@users.noreply.github.com>
2021-05-13 09:17:16 +0200

    Add files via upload

---
+++ includes/cookie-inc.php
@@ -0,0 +1,15 @@
+<div class="modal fade" id="cookieModal">
+  <div class="modal-dialog cookie-modal-dialog m-0">
+    <div class="modal-content cookie-modal-content pl-5 pr-5">
+      <div class="modal-body cookie-modal-body wraper">
+        <div class="notice d-flex justify-content-between align-items-center">
+          <div class="cookie-text p-2 "><p class="mb-0 text-center font-slabo"><strong>Ta strona wykorzystuje pliki cookies</strong>.<br> Używamy informacji zapisanych za pomocą plików cookies w celu zapewnienia maksymalnej wygody w korzystaniu z naszego serwisu. Klikając "Akceptuję" wyrażasz zgodę na ich użycie. Jeśli nie wyrażasz zgody, ustawienia dotyczące plików cookies możesz zmienić w swojej przeglądarce.</p></div>
+        </div>
+         <div class="text-center">
+          <button type="button" onclick="setCookie()" class="btn appointment-btn margin-btn" id="ModalCookieClose" class="close" data-dismiss="modal" aria-hidden="true">Akceptuję</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
